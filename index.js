3456b1b725a0761148e848b77d38ba499ebcc60b
odinzv69 <125555046+odinzv69@users.noreply.github.com>
2023-04-03 16:07:26 +0300

    домашка--12

--- index.js
+++ index.js
@@ -1,101 +1,124 @@
-
 const buttonInputElement = document.querySelector('.add-form-button');
 const nameInputElement = document.querySelector('.add-form-name');
 const commentInputElement = document.querySelector('.add-form-text');
 const listElement = document.querySelector('.comments');
 buttonInputElement.disabled = true;
+let arrayOfComments=[];
 
-const сomments = [
-  {
-  name : 'Глеб Фокин',
-  time : '12.02.23 12:18',
-  text : 'Это будет первый комментарий на этой странице',
-  'likes-counter' : 3, 
-  'likes-class' : '',
-  },
+reload();
 
-{
-  name : 'Варвара Н.',
-  time : '13.02.22 19:22',
-  text : 'Мне нравится как оформлена эта страница! ❤',
-  'likes-counter' : 75, 
-  'likes-class' : '-active-like',
+  function reload() {
 
-},
-  ];
+    fetch("https://webdev-hw-api.vercel.app/api/v1/egor-zuev/comments", {
+      method: "GET"
+    }).then((response) => {
+          response.json().then((responseData) => {            
 
-function renderComment() {
-const listElement = document.querySelector('.comments');
+            arrayOfComments = responseData.comments.map(el => {
+                  
+
+                   return{
+                      name: el.author.name,
+                      time: myDate(el.date),
+                      text: el.text,
+                      'likes-counter': el.likes,
+                      'likes-class': el.isLiked == true ? '-active-like' : '',
+
+                  };
+                
+                });    
+                
+                renderComment();
+
+                likes();
+                
+            });
+      });
+    };
+
+  function renderComment() {
+
+  const listElement = document.querySelector('.comments');
   
 
-listElement.innerHTML = сomments.map((el,index)=> el=`
-      <li class="comment" data-index='${index}'>
-        <div class="comment-header">
-          <div class="comment-author">${el.name}</div>
-          <div class="comment-time">${el.time}</div>
-        </div>
-        <div class="comment-body">
-          <div class="comment-text">${el.text}</div>
-        </div>
-        <div class="comment-footer">    
-          <div class="likes">
-            <span class="likes-counter">${el['likes-counter']}</span>
-            <button class="like-button ${el['likes-class']}"></button>
+  listElement.innerHTML = arrayOfComments.map((el,index)=> el=`
+        <li class="comment" data-index='${index}'>
+          <div class="comment-header">
+            <div class="comment-author">${el.name}</div>
+            <div class="comment-time">${el.time}</div>
           </div>
-        </div>
-      </li>`).join('');
+          <div class="comment-body">
+            <div class="comment-text">
+              ${el.text}
+            </div>
+          </div>
+          <div class="comment-footer">
+            
+            <div class="likes">
+              <span class="likes-counter">${el['likes-counter']}</span>
+              <button class="like-button ${el['likes-class']}"></button>
+            </div>
+          </div>
+        </li>`).join('');    
 
   likes();
 
   editComment();
         
-}
+  }
 
-renderComment();
+  function checkValue() {
+    if( nameInputElement.value != '' && commentInputElement.value != '') buttonInputElement.disabled = false; 
+  }
 
-function checkValue() {
-  if( nameInputElement.value != '' && commentInputElement.value != '') buttonInputElement.disabled = false; 
-}
+  checkValue();
 
-checkValue();
+  buttonInputElement.addEventListener('click',() => {
 
-buttonInputElement.addEventListener('click',() => {
-  if(nameInputElement.value == '' || commentInputElement.value == '') return;
+    if(nameInputElement.value == '' || commentInputElement.value == '') return;
 
-nameInputElement.value = nameInputElement.value.replaceAll("&", "&amp;")
-                                               .replaceAll("<", "&lt;")
-                                               .replaceAll(">", "&gt;");
+    nameInputElement.value = nameInputElement.value.replaceAll("&", "&amp;")
+                                                   .replaceAll("<", "&lt;")
+                                                   .replaceAll(">", "&gt;");
         
-commentInputElement.value = commentInputElement.value.replaceAll("&", "&amp;")
-                                                     .replaceAll("<", "&lt;")
-                                                     .replaceAll(">", "&gt;")
-                                                     .replaceAll('◄', '<div class ="quote">')
-                                                     .replaceAll('►', '</div><br>');
-      
-сomments.push({name : nameInputElement.value,
-                      time : myDate(),
-                      text : commentInputElement.value,
-                      'likes-counter' : 0, 
-                      'likes-class' : '',
+    commentInputElement.value = commentInputElement.value.replaceAll("&", "&amp;")
+                                                         .replaceAll("<", "&lt;")
+                                                         .replaceAll(">", "&gt;")
+                                                         .replaceAll('◄', '<div class ="quote">')
+                                                         .replaceAll('►', '</div><br>');
+
+
+      fetch("https://webdev-hw-api.vercel.app/api/v1/egor-zuev/comments", {
+      method: "POST",
+      body: JSON.stringify({ 
+                text: commentInputElement.value,
+                name: nameInputElement.value,
+                likes : 'likes-counter',
+                })
+      }).then((response) => {
+        response.json().then((responseData) => {
+          
+          reload();                                                         
+          
+        });
+     });
+
+      nameInputElement.value = '';
+      commentInputElement.value = '';
+      buttonInputElement.disabled = true;
   });
 
-  renderComment();
-  nameInputElement.value = '';
-  commentInputElement.value = '';
-  buttonInputElement.disabled = true;
-});
-
-function myDate () {
-    let date = new Date();
-    let monthArray=['01','02','03','04','05','06','07','08','09','10','11','12'];
-    let myMinutes = String(date.getMinutes()).length < 2 ? '0' + date.getMinutes() : date.getMinutes();
-    let myHours = String(date.getHours()).length < 2 ? '0' + date.getHours() : date.getHours();
-    let myDay = String(date.getDate()).length < 2 ? '0' + date.getDate() : date.getDate();
-    let myMonth = monthArray[+date.getMonth()];
-    let myYear = String( date.getFullYear() ).slice(2);
-    let str= myDay + '.' + myMonth + '.' + myYear + ' ' + myHours + ':' + myMinutes;
-    return str;
-}
+  function myDate (a) {
+      let date = new Date(a) || new Date();
+      let monthArray=['01','02','03','04','05','06','07','08','09','10','11','12'];
+      let myMinutes = String(date.getMinutes()).length < 2 ? '0' + date.getMinutes() : date.getMinutes();
+      let myHours = String(date.getHours()).length < 2 ? '0' + date.getHours() : date.getHours();
+      let myDay = String(date.getDate()).length < 2 ? '0' + date.getDate() : date.getDate();
+      let myMonth = monthArray[+date.getMonth()];
+      let myYear = String( date.getFullYear() ).slice(2);
+      let str= myDay + '.' + myMonth + '.' + myYear + ' ' + myHours + ':' + myMinutes;
+      return str;
+  }
 
   function likes(){
  
@@ -127,7 +150,7 @@ function myDate () {
             let author = key.querySelector('.comment-author');
             let text = key.querySelector('.comment-text');
             commentInputElement.value = `◄ ${author.textContent} ${text.textContent} ►`;
-                        
+
           }); 
         }
       }
